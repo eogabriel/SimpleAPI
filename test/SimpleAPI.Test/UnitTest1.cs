70452093b0184dc3af84a18900bd268eb0c71654
Elvis Gabriel <elvis.gabriel@doctorcareanywhere.com>
2020-07-02 17:22:22 +0100

    initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using SimpleAPI.Controllers;
+using System.Linq;
+using Xunit;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void Get_ReturnsListOfWeatherForecasts()
+        {
+            var result = controller.Get();
+
+            Assert.Equal(5, result.Count());
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
